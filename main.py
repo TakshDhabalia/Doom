bf0c69ed77a97f67676d63e7278c2ce9db1fb045
Taksh <dhabalia.taksh@gmail.com>
2024-02-11 23:02:42 +0530

    add player file and make the 2D mesh with user input control with WASD and up/down work (after a lot of time) and also also add a mesh structure for the screen as POC and will look for it to make it 3D

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import moderngl
 import sys
 from settings import *
 from map import *
+from player import *
+
 
 #my day one documentation is going to be so bad cause i just made a mesh rn 
 
@@ -13,19 +15,23 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode(RES)#resolution from settings file
         self.clock = pg.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)#to initialize the map and to put the basic vals in
-    
+        self.player = Player(self)
+
     def update(self):
+        self.player.update()
         pg.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() : .1f}')
 
     def draw(self):
         self.screen.fill('black')#bg fill
         self.map.draw()
+        self.player.draw()
 
     def check_events(self):#to add the main exit loop and its basically esc or ctrl+c
         for event in pg.event.get():
