d0a51f695c0b5f1e9ce6a69c07709a09edfaf43d
Taksh <dhabalia.taksh@gmail.com>
2024-02-17 15:46:50 +0530

    add collision control on world map

--- player.py
+++ player.py
@@ -33,8 +33,7 @@ class Player:
             num_key_pressed += 1
             dx += -speed_sin
             dy += speed_cos
-        self.x += dx
-        self.y += dy
+        self.check_wall_collision_movement(dx, dy)
         if keys[pg.K_LEFT]:
              self.angle -= PLAYER_ROT_SPEED * self.game.delta_time
         if keys[pg.K_RIGHT]:
@@ -47,7 +46,15 @@ class Player:
                     self.y * 100 + WIDTH * math. sin(self.angle)), 2)
         pg.draw.circle(self.game.screen, 'green', (self.x * 100, self.y * 100), 15)
     
-
+    def check_wall(self, x, y ):
+        return (x,y) not in self.game.map.world_map
+    
+    def check_wall_collision_movement(self, dx, dy):
+        if self.check_wall(int(self.x+ dx) , int(self.y)):
+            self.x = self.x + dx 
+        if self.check_wall( int(self.x), int(self.y+ dy) ):
+            self.y = self.y + dy
+        
     def update(self):
         self.movement()
     
