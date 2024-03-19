dcf8ee93ff77447d08ac4d7cc9d84d9beb32dae0
Taksh <dhabalia.taksh@gmail.com>
2024-03-19 21:46:51 +0530

    bapu moment

--- npc.py
+++ npc.py
@@ -208,7 +208,16 @@ class CyberDemonNPC(NPC):
         self.attack_damage = 15
         self.speed = 0.055
         self.accuracy = 0.25
-
+class Bapu(NPC):
+    def __init__ (self, game, path='resources/sprites/npc/bapu_moment/0.png', pos=(11.5, 6.0),
+                 scale=1.0, shift=0.04, animation_time=210):
+        super().__init__(game , path , pos , scale, shift , animation_time)
+        self.attack_dist = 6
+        self.health = 350
+        self.attack_damage = 15
+        self.speed = 0.055
+        self.accuracy = 0.25
+        
 
 
 
--- object_handler.py
+++ object_handler.py
@@ -17,8 +17,8 @@ class ObjectHandler:
 
         # spawn npc
         self.enemies = 20  # npc count
-        self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC]
-        self.weights = [70, 20, 10]
+        self.npc_types = [SoldierNPC, CacoDemonNPC, CyberDemonNPC, Bapu]
+        self.weights = [0, 0, 0, 100]
         self.restricted_area = {(i, j) for i in range(10) for j in range(10)}
         self.spawn_npc()
 
