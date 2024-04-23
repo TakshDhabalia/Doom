c837e263af461d6cc276b3520fb22e831f95cc04
Taksh <dhabalia.taksh@gmail.com>
2024-04-23 22:36:06 +0530

    fixed sound

--- sound.py
+++ sound.py
@@ -12,5 +12,5 @@ class Sound:
         self.npc_shot = pg.mixer.Sound(self.path + 'npc_attack.wav')
         self.npc_shot.set_volume(0.2)
         self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
-        self.theme = pg.mixer.music.load(self.path + 'Ala-Baburao-Ala-Baburao.wav')
+        self.theme = pg.mixer.music.load(self.path + 'theme.mp3')
         pg.mixer.music.set_volume(0.3)
